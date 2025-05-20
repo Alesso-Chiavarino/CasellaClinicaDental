f98884877e433bb80808b0ee1087db143ff15120
alesso-chiavarino <alessochiavarino@gmail.com>
2025-05-20 18:31:07 -0300

    add testimonials

--- src/common/SectionTitle/SectionTitle.tsx
+++ src/common/SectionTitle/SectionTitle.tsx
@@ -9,7 +9,7 @@ export const SectionTitle = ({ title, description }: ISectionTitleProps) => {
       className="flex flex-col items-center justify-center gap-4 px-4 py-20 mx-auto text-center"
     >
       <h2 className="text-4xl font-bold">{title}</h2>
-      <p className="text-lg font-medium opacity-80 w-[70%]">{description}</p>
+      {description && <p className="text-lg font-medium opacity-80 w-[70%]">{description}</p>}
     </div>
   );
 };
