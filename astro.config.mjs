563ea7c410c44a68eee8b35b0cd1a078adf624cf
alesso-chiavarino <alessochiavarino@gmail.com>
2025-05-22 15:56:21 -0300

    Add sitemap integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,14 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import flowbiteReact from "flowbite-react/plugin/astro";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
 
-  integrations: [react(), flowbiteReact()],
+  integrations: [react(), flowbiteReact(), sitemap()],
   adapter: vercel(),
 });
