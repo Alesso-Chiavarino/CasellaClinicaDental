4d6f0c5d0b01714e5f1ca6339082bf04a7fb4038
alesso-chiavarino <alessochiavarino@gmail.com>
2025-05-22 10:10:39 -0300

    Add animation attributes to elements, update service card structure, and apply fade-up animation to testimonials.

--- src/components/Services/ServiceCard/ServiceCard.tsx
+++ src/components/Services/ServiceCard/ServiceCard.tsx
@@ -1,29 +1,27 @@
 interface IServiceCardProps {
     service: {
-        icon: React.ReactNode
-        title: string
-        description: string
-    },
+      icon: React.ReactNode;
+      title: string;
+      description: string;
+    };
     idx: number;
-}
-
-export const ServiceCard = ({ service, idx }: IServiceCardProps) => {
+  }
+  
+  export const ServiceCard = ({ service, idx }: IServiceCardProps) => {
     return (
-       <li 
-       key={service.title}
-      data-aos="fade-up"
-      data-aos-duration="600"
-      data-aos-delay={idx * 100} // 0ms, 100ms, 200ms, etc.
-      data-aos-easing="ease-out-sine"
-       className="bg-white shadow-md rounded-xl p-6 hover:shadow-lg transition duration-300">
-            <div className="flex items-center gap-3 mb-2 h-15">
-                <div className="text-[#b8eae9]">
-                    {service.icon}
-                </div>
-                <h3 className="text-2xl font-semibold text-gray-600">{service.title}</h3>
-            </div>
-
-            <p className="text-md">{service.description}</p>
-        </li>
-    )
-}
+      <li
+        key={service.title}
+        data-aos="fade-up"
+        data-aos-duration="600"
+        data-aos-delay={idx * 100}
+        data-aos-easing="ease-out-sine"
+        className="bg-white shadow-md rounded-xl p-6 hover:shadow-lg transition duration-300"
+      >
+        <div className="flex items-center gap-3 mb-2 h-15">
+          <div className="text-[#b8eae9]">{service.icon}</div>
+          <h3 className="text-2xl font-semibold text-gray-600">{service.title}</h3>
+        </div>
+        <p className="text-md">{service.description}</p>
+      </li>
+    );
+  };
